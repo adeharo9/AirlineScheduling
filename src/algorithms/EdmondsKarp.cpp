53006f4d9bc63d9f0b0429a3bba7ed62f53c2ad3
sergi-as <chechi.as@hotmail.com>
2018-01-04 15:21:49 +0100

    recupera solucio

--- src/algorithms/EdmondsKarp.cpp
+++ src/algorithms/EdmondsKarp.cpp
@@ -1,9 +1,36 @@
 // C++ program for implementation of Edmonds Karp algorithm
 
 #include "EdmondsKarp.h"
+
+void print_sol(const vector<vector<int>> &g,const vector<vector<int>> &ini){
+	cout<<"Resultat: "<<endl<<endl;
+	//si s'ha seguit una aresta amb capacitat 1, al graf final te capacitat 0
+	int p=1;
+	int size=g.size();
+	for(int i=4;i<size;i+=2){//origen
+		if (g[2][i]==0){//si el camí comença per el vertex i
+			cout<<"Pilot: "<<p<<endl;
+			int j=i;
+			//index segons l'exemple de l'enunciat
+			while(g[j+1][3]!=0){// si NO es l'ultim vol que fa el pilot
+				cout<<"	Vol: "<<(j-4)/2+1<<endl;
+				bool found=false;
+				for(int k=4;k<size and not found;k+=2){
+					if(g[j+1][k]==0 and ini[j+1][k]==1){//si el vol ha seguit per aquest camí
+						j=k;
+						found=true;
+					}
+				}
+			}
+			cout<<"        Vol: "<<(j-4)/2+1<<endl<<endl;
+			p++;
+		}
+	}
+}
+
 /* Returns true if there is a path from source 's' to sink 't' in
   residual graph. Also fills parent[] to store the path */
-bool bfs(const vector<vector<int>> rGraph, int s, int t, int parent[],int V)
+bool bfs(const vector<vector<int>> &rGraph, int s, int t, int parent[],int V)
 {
 	// Create a visited array and mark all vertices as not visited
 	bool visited[V];
@@ -39,7 +66,7 @@ bool bfs(const vector<vector<int>> rGraph, int s, int t, int parent[],int V)
 }
 
 // Returns the maximum flow from s to t in the given graph
-int EdmondsKarp(vector<vector<int>> graph, int s, int t,int V)
+int EdmondsKarp(vector<vector<int>> & graph, int s, int t,int V)
 {
 	int u, v;
 
@@ -51,8 +78,11 @@ int EdmondsKarp(vector<vector<int>> graph, int s, int t,int V)
 					 // residual capacity of edge from i to j (if there
 					 // is an edge. If rGraph[i][j] is 0, then there is not)
 	for (u = 0; u < V; u++){
+		if (u==4)cout<<"----------------------"<<endl;
 
 		for (v = 0; v < V; v++){
+			if (v==4)cout<<'|';
+
 			 rGraph[u][v] = graph[u][v];
 			 cout<<graph[u][v]<<" ";
 		   }
@@ -88,18 +118,27 @@ int EdmondsKarp(vector<vector<int>> graph, int s, int t,int V)
 		max_flow += path_flow;
 	}
 	// Return the overall flow
+// TODO hacer una funcion para print y que no sea tan cutre
 
-	//para comprobar
-	int pilotos=0;
-	cout<<"final del S con demandas"<<endl;
-	for (int i=0;i<V;i++){
-	  cout<<rGraph[2][i]<<" ";
-	  if (rGraph[2][i]==1) pilotos++;
+	cout<<"Final"<<endl<<endl;
+	for (u = 0; u < V; u++){
+		if (u==4)cout<<"----------------------"<<endl;
+		for (v = 0; v < V; v++){
+			if (v==4)cout<<'|';
+			 cout<<rGraph[u][v]<<" ";
+		   }
+			 cout<<endl;
 	}
-	cout<<endl<<"final del S sin demandas"<<endl;
-	for (int i=0;i<V;i++){
-	  cout<<rGraph[0][i]<<" ";
-	  if (rGraph[0][i]==1) pilotos++;
+
+	//si el resultat es solució, el flow al T real ha de ser k + nombre de vols
+	int k=graph[3][1]; //un lloc on es troba k
+	int nvols=(graph.size()-4)/2;
+	int flow=0;
+	if (rGraph[3][1]==0)flow=k;
+	for (int i=4;i<graph.size();i+=2){
+		if(rGraph[i][1]==0)flow++;
 	}
+	if (flow==nvols+k) print_sol(rGraph,graph);
+	else cout<<"No s'ha trobat cap solució"<<endl;
 	return max_flow;
 }
--- src/algorithms/EdmondsKarp.h
+++ src/algorithms/EdmondsKarp.h
@@ -9,15 +9,16 @@
 
 using namespace std;
 
-// Number of vertices in given graph
+// Traces back and prints the solution
 
+void print_sol(const vector<vector<int>> & g,const vector<vector<int>> &ini);
 
 /* Returns true if there is a path from source 's' to sink 't' in
   residual graph. Also fills parent[] to store the path */
-bool bfs(const vector<vector<int>> rGraph , int s, int t, int parent[],int V);
+bool bfs(const vector<vector<int>> &rGraph , int s, int t, int parent[],int V);
 
 // Returns the maximum flow from s to t in the given graph
-int EdmondsKarp(vector<vector<int>> rGraph, int s, int t,int V);
+int EdmondsKarp(vector<vector<int>> &rGraph, int s, int t,int V);
 
 
 #endif //AIRLINESCHEDULING_EDMONDSKARP_H
