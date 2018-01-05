eef1a25d284fb35b4eaf542f16ecd8fc4cfc621e
Alejandro de Haro <a.deharo9@gmail.com>
2018-01-05 17:06:17 +0100

    Refactors variables

--- src/AirlineScheduling.cpp
+++ src/AirlineScheduling.cpp
@@ -14,23 +14,23 @@ using namespace std;
 	per tot node destí comprobem per tot node origen si es possible fer transició
 	cost n * n on n son els vols
 */
-void version1 (Graph& data)
+void version1 (Graph& graph)
 {
-	for (uint i = Graph::firstDestinationVertex; i < data.vertexSize(); i += 2)	// i es desti
+	for (uint i = Graph::firstDestinationVertex; i < graph.vertexSize(); i += 2)	// i es desti
 	{
-		for (uint j = Graph::firstOriginVertex; j < data.vertexSize(); j += 2)	// j es origen
+		for (uint j = Graph::firstOriginVertex; j < graph.vertexSize(); j += 2)	// j es origen
 		{
 			if (j != i - 1)	// per evitar fer calculs sobre el mateix vol
 			{
-				if (data.getVertex(i).getCity() == data.getVertex(j).getCity() and int(data.getVertex(j).getTime()) - int(data.getVertex(i).getTime()) >= MIN_TRANSITION_TIME)
+				if (graph.getVertex(i).getCity() == graph.getVertex(j).getCity() and int(graph.getVertex(j).getTime()) - int(graph.getVertex(i).getTime()) >= MIN_TRANSITION_TIME)
 				{
 					if (i == 7 and j == 4)
 					{
-						cout << "Aqui estoy, con tiempo: " << data.getVertex(j).getTime() - data.getVertex(i).getTime() << " " << MIN_TRANSITION_TIME << endl;
-						cout << "n j " << data.getVertex(j).getTime() << " n i " << data.getVertex(i).getTime() << endl;
+						cout << "Aqui estoy, con tiempo: " << graph.getVertex(j).getTime() - graph.getVertex(i).getTime() << " " << MIN_TRANSITION_TIME << endl;
+						cout << "n j " << graph.getVertex(j).getTime() << " n i " << graph.getVertex(i).getTime() << endl;
 					}
 					// aresta del desti i al origen j amb pes 1
-					data.addNeighbour(i, Edge(i, j, 1));
+					graph.addNeighbour(i, Edge(i, j, 1));
 				}
 			}
 		}
@@ -40,17 +40,17 @@ void version1 (Graph& data)
   elimina els lower bound de les arestes i actualitza les demandes
   les uniques arestes amb lower suerten de les ciutats d'origen
 */
-void deleteLowerBound(Graph &data)
+void deleteLowerBound(Graph &graph)
 {
-	for (uint i = Graph::firstOriginVertex; i < data.vertexSize(); i += Graph::originStepSize)
+	for (uint i = Graph::firstOriginVertex; i < graph.vertexSize(); i += Graph::originStepSize)
 	{
 		//la primera aresta es la del viatge
-		data.getEdge(i, Graph::sourceWithoutDemand).setCapacity(data.getEdge(i, Graph::sourceWithoutDemand).getCapacity() - data.getEdge(i, Graph::sourceWithoutDemand).getLowerBound());
-		data.getEdge(i, Graph::sourceWithoutDemand).setLowerBound(0);
+		graph.getEdge(i, Graph::sourceWithoutDemand).setCapacity(graph.getEdge(i, Graph::sourceWithoutDemand).getCapacity() - graph.getEdge(i, Graph::sourceWithoutDemand).getLowerBound());
+		graph.getEdge(i, Graph::sourceWithoutDemand).setLowerBound(0);
 
 		//sumo 1 a la demanda de l'origen, i resto 1 a la de destí
-		data.getVertex(i).incrementDemand();
-		data.getVertex(i + 1).decrementDemand();
+		graph.getVertex(i).incrementDemand();
+		graph.getVertex(i + 1).decrementDemand();
 	}
 }
 
@@ -58,32 +58,32 @@ void deleteLowerBound(Graph &data)
   elimina les demandes  i actualizta el sink i drain reals amb les
   arestes necessaries
 */
-void deleteDemand(Graph &data)
+void deleteDemand(Graph &graph)
 {
-	for (uint i = Graph::firstFlowNetworkVertex; i < data.vertexSize(); ++i)
+	for (uint i = Graph::firstFlowNetworkVertex; i < graph.vertexSize(); ++i)
 	{
-		if (data.getVertex(i).getDemand() > 0)	// Si demanda > 0 es un sink i va a t
+		if (graph.getVertex(i).getDemand() > 0)	// Si demanda > 0 es un sink i va a t
 		{
-			data.addNeighbour(i, Edge(i, 1, data.getVertex(i).getDemand()));
-			data.getVertex(i).setDemand(0);
+			graph.addNeighbour(i, Edge(i, 1, graph.getVertex(i).getDemand()));
+			graph.getVertex(i).setDemand(0);
 		}
-		else if (data.getVertex(i).getDemand() < 0)	// Si es source s va a el
+		else if (graph.getVertex(i).getDemand() < 0)	// Si es source s va a el
 		{
-			data.addNeighbour(Graph::sourceWithoutDemand, Edge(Graph::sourceWithoutDemand, i, -data.getVertex(i).getDemand()));
-			data.getVertex(i).setDemand(0);
+			graph.addNeighbour(Graph::sourceWithoutDemand, Edge(Graph::sourceWithoutDemand, i, -graph.getVertex(i).getDemand()));
+			graph.getVertex(i).setDemand(0);
 		}
 	}
 }
 
-void transformMax(Graph &data)
+void transformMax(Graph &graph)
 {
-	vector<vector<int>> graph(data.vertexSize(), vector<int>(data.vertexSize(), 0));
+	vector<vector<int>> adjacenceMatrixGraph(graph.vertexSize(), vector<int>(graph.vertexSize(), 0));
 
-	for(uint i = Graph::firstElement; i < data.vertexSize(); ++i)
+	for(uint i = Graph::firstElement; i < graph.vertexSize(); ++i)
 	{
-		for (uint j = 0; j < data.getEdges(i).size(); ++j)
+		for (uint j = 0; j < graph.getEdges(i).size(); ++j)
 		{
-			graph[i][data.getEdge(i, j).getDestination()] = data.getEdge(i, j).getCapacity();
+			adjacenceMatrixGraph[i][graph.getEdge(i, j).getDestination()] = graph.getEdge(i, j).getCapacity();
 		}
 	}
 
@@ -92,14 +92,14 @@ void transformMax(Graph &data)
 	FordFulkerson* edmondsKarp = new EdmondsKarp();
 	Chrono chrono;
 	chrono.start(0);
-	cout << endl << edmondsKarp -> algorithm(graph, 0, 1, data.vertexSize()) << endl;
+	cout << endl << edmondsKarp -> algorithm(adjacenceMatrixGraph, 0, 1, graph.vertexSize()) << endl;
 	chrono.stop(0);
 	cout << "Chrono duration: " << chrono.duration(0) << endl;
 	delete edmondsKarp;
 
 	FordFulkerson* fordFulkersonDFS = new FordFulkersonDFS();
 	chrono.start(1);
-	cout << endl << fordFulkersonDFS -> algorithm(graph, 0, 1, data.vertexSize()) << endl;
+	cout << endl << fordFulkersonDFS -> algorithm(adjacenceMatrixGraph, 0, 1, graph.vertexSize()) << endl;
 	chrono.stop(1);
 	cout << "Chrono duration: " << chrono.duration(1) << endl;
 	delete fordFulkersonDFS;
@@ -113,9 +113,9 @@ int main ()
 	uint arrivalTime;
 
 	// estructura de dades per guardar les arestes
-	Graph data(57);
+	Graph graph(57);
 
-	uint i = 4;
+	uint maxFlights = 0;
 
 	while (cin >> origin)	// operacions descrites a la pagina 390 del llibre
 	{
@@ -126,17 +126,14 @@ int main ()
 		Vertex vorigin(origin, departureTime);
 		Vertex vdestination(destination, arrivalTime);
 
-		data.addEdge(vorigin, vdestination);
+		graph.addEdge(vorigin, vdestination);
 
-		i += 2;
+		++maxFlights;
 	}
 
-	version1(data);			// Afegir arestes si es pot arribar d'un vol a un altre
-	deleteLowerBound(data);	// Eliminar els lower bound
-	deleteDemand(data);		// Eliminar les demandes
+	version1(graph);			// Afegir arestes si es pot arribar d'un vol a un altre
+	deleteLowerBound(graph);	// Eliminar els lower bound
+	deleteDemand(graph);		// Eliminar les demandes
 
-	transformMax(data);
-
-	data.updateMaxFlights(57);
-	transformMax(data);
+	transformMax(graph);
 }
