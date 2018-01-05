27a4447d749063ee1365a5c436dc95b9ce1777c3
Alejandro de Haro <a.deharo9@gmail.com>
2018-01-05 04:20:07 +0100

    Adds graph data structure

--- src/structures/Graph.cpp
+++ src/structures/Graph.cpp
@@ -1,1 +1,50 @@
 #include "Graph.h"
+
+/* CONSTRUCTOR */
+
+Graph::Graph() : lastEmplaced(minElements), edges(minElements), vertices(minElements)
+{
+
+}
+
+/* MODIFY METHODS */
+
+void Graph::addEdge(const Vertex &origin, const Vertex &destination, int capacity, int lowerBound)
+{
+	if(capacity < 0) throw invalid_argument("capacity");
+	if(lowerBound < 0) throw invalid_argument("lowerBound");
+
+	uint originIndex = lastEmplaced;
+	uint destinationIndex = originIndex + 1;
+
+	vertices.emplace_back(origin);
+	vertices.emplace_back(destination);
+
+	edges[sourceWithDemand].emplace_back(Edge(sourceWithDemand, originIndex, minFlowValue, maxFlowValue));
+	edges.emplace_back(vector<Edge>(1, Edge(originIndex, destinationIndex, uint(capacity), uint(lowerBound))));
+	edges.emplace_back(vector<Edge>(1, Edge(destinationIndex, sinkWithDemand, minFlowValue, maxFlowValue)));
+
+	lastEmplaced += 2;
+}
+
+void Graph::addNeighbour(uint index, const Edge &destination)
+{
+	edges[index].emplace_back(destination);
+}
+
+/* GET METHODS */
+
+Vertex& Graph::getVertex(uint index)
+{
+	return this -> vertices[index];
+}
+
+Edge& Graph::getEdge(uint indexI, uint indexJ)
+{
+	return this -> edges[indexI][indexJ];
+}
+
+uint Graph::vertexSize() const
+{
+	return this -> vertices.size();
+}
--- src/structures/Graph.h
+++ src/structures/Graph.h
@@ -2,8 +2,60 @@
 #define AIRLINESCHEDULING_GRAPH_H
 
 
+#include <unordered_map>
+#include <vector>
+
+#include "Vertex.h"
+#include "Edge.h"
+
+using namespace std;
+
+typedef unsigned int uint;
+
 class Graph
 {
+public:
+
+	/* ATTRIBUTES */
+
+	static const uint sourceWithoutDemand = 0;
+	static const uint sinkWithoutDemand = sourceWithoutDemand + 1;
+	static const uint sourceWithDemand = sinkWithoutDemand + 1;
+	static const uint sinkWithDemand = sourceWithDemand + 1;
+
+	static const uint firstVertex = sinkWithDemand + 1;
+
+private:
+
+	static const uint minFlowValue = 0;
+	static const uint maxFlowValue = 1;
+
+	static const uint minElements =  sinkWithDemand + 1;
+
+	uint lastEmplaced;
+
+	vector<vector<Edge>> edges;
+	vector<Vertex> vertices;
+
+public:
+
+	/* CONSTRUCTORS */
+
+	Graph();
+
+	/* MODIFY METHODS */
+
+	void addEdge(const Vertex &origin, const Vertex &destination, int capacity = 0, int lowerBound = 0);
+
+	void addNeighbour(uint index, const Edge &destination);
+
+	/* GET METHODS */
+
+	Vertex& getVertex(uint index);
+
+	Edge& getEdge(uint indexI, uint indexJ);
+
+	uint vertexSize() const;
 
 };
 
